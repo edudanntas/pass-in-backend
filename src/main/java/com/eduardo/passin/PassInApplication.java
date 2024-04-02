813dbf938d663dcf5f32768086245cd979e52616
Eduardo Dantas <eduardodanttas@icloud.com>
2024-04-02 20:51:28 -0300

    Configure database, create migrations
    - Configure database URL on applications.properties
    - Create Migration to create events table
    - Create Migration to create attendees table
    - Create Migration to create checkins table
    - Create Index for slug on event to prevent duplicate
    - Create Index for attendee on attendee to prevent duplicate register
    - Create Index for checkin to prevent duplicate chekin on events

--- src/main/java/com/eduardo/passin/PassInApplication.java
+++ src/main/java/com/eduardo/passin/PassInApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PassInApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PassInApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PassInApplication.class, args);
+    }
 
 }
