4e71fc06b20f69c081419bf240b7e897929eaf74
Eduardo Dantas <eduardodanttas@icloud.com>
2024-04-02 21:16:40 -0300

    Create entities
    - Create Event entity
    - Create Attendee entity
    - Create CheckIn entity

---
+++ src/main/java/com/eduardo/passin/domain/entities/CheckIn.java
@@ -0,0 +1,26 @@
+package com.eduardo.passin.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
