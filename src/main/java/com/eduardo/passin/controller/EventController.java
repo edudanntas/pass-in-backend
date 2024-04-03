4a6bce03492e6779030516f2a439a988c9eb210c
Eduardo Dantas <eduardodanttas@icloud.com>
2024-04-03 08:15:13 -0300

    Create event Controller
    - Create `/events` endpoint to make a POST request to create an Event and get a URI on header
    - Create `/events/id` endpoint do get a specific event

---
+++ src/main/java/com/eduardo/passin/controller/EventController.java
@@ -0,0 +1,34 @@
+package com.eduardo.passin.controller;
+
+import com.eduardo.passin.dto.event.EventIdDTO;
+import com.eduardo.passin.dto.event.EventRequestDTO;
+import com.eduardo.passin.dto.event.EventResponseDTO;
+import com.eduardo.passin.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEventDetails(@PathVariable String id){
+        EventResponseDTO event = this.service.getEventDetails(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO requestDTO, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.service.createEvent(requestDTO);
+
+        URI uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.id()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+}
