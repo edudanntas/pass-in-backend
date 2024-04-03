a536d60daa9b0c826cf6845039e27c6966c2cbba
Eduardo Dantas <eduardodanttas@icloud.com>
2024-04-03 08:12:59 -0300

    Create event Service
    - Create getEventDetails method to return a detailed event
    - Create Slug creator with normalized method
    - Create a method to create a Event with DTO

---
+++ src/main/java/com/eduardo/passin/services/EventService.java
@@ -0,0 +1,48 @@
+package com.eduardo.passin.services;
+
+import com.eduardo.passin.domain.entities.Attendee;
+import com.eduardo.passin.domain.entities.Event;
+import com.eduardo.passin.dto.event.EventIdDTO;
+import com.eduardo.passin.dto.event.EventRequestDTO;
+import com.eduardo.passin.dto.event.EventResponseDTO;
+import com.eduardo.passin.repositories.AttendeeRepository;
+import com.eduardo.passin.repositories.EventRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+    private final AttendeeRepository attendeeRepository;
+
+    public EventResponseDTO getEventDetails(String eventId){
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EntityNotFoundException("Not found event with id:" + eventId));
+        List<Attendee> attendees = attendeeRepository.findByEventId(eventId);
+        return new EventResponseDTO(event, attendees.size());
+    }
+
+    public EventIdDTO createEvent(EventRequestDTO requestDTO){
+        Event newEvent = new Event();
+        newEvent.setTitle(requestDTO.title());
+        newEvent.setDetails(requestDTO.details());
+        newEvent.setMaximumAttendees(requestDTO.maximumAttendees());
+        newEvent.setSlug(this.createSlug(requestDTO.title()));
+
+        this.eventRepository.save(newEvent);
+        return new EventIdDTO(newEvent.getId());
+    }
+
+    private String createSlug(String text){
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
+    }
+}
